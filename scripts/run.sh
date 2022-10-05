280ad6e3977abe145c24a19e3a28241e6a42beb9
Menghao Yu <68170909+menghaoyu2002@users.noreply.github.com>
2022-10-05 19:35:00 -0400

    Fix typo in word "application"

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,8 @@
-echo 'starting the applicatoin...'
+echo 'starting the application...'
 {
 	docker run --rm --name team_rocket_app team_rocket_app
 } || {
-	echo 'Failed. There was an issue running the applicatoin with its docker image.'
+	echo 'Failed. There was an issue running the application with its docker image.'
 	echo 'Please report the issue to https://github.com/menghaoyu2002/CSC302-TeamRocket/issues with your terminal output'
 	exit 1
 }
