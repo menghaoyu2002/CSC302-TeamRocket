0465ac9e830637c68a0c2f7a5630e12f84be1d20
Menghao Yu <menghaoyu2002@gmail.com>
2022-10-23 12:38:07 -0400

    update docker to run flask server

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,9 @@ FROM python:3
 
 WORKDIR /usr/src/app
 
-# TODO: intall dependencies when we use them.
-# Currently there is not requirements.txt file so this will cause the build to break.
-# COPY requirements.txt ./
-# RUN pip install --no-cache-dir -r requirements.txt
+COPY requirements.txt ./
+RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-CMD [ "python", "./app/app.py" ]
+CMD [ "python", "-m", "flask", "--app", "./app/app.py", "run", "--host","0.0.0.0"]
--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
-from databasemanager import DatabaseManager
 from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
@@ -10,15 +10,10 @@ def index():
     """
     return "<p>Index Page of the website</p>"
 
+
 @app.route("/home")
 def home_page():
     """
     Create the home page for the website.
     """
     return "<p>This is the home page! Make sure the other routes are in different files!</p>"
-
-if __name__ == "__main__":
-    db_helper = DatabaseManager()
-    db_helper.get_all_data()
-    db_helper.close_connection()
-    
