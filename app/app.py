dfe3345485b8c2a4734b2fb9ccbff06680dd0c26
Michael Kwan <michaelk.kwan@mail.utoronto.ca>
2022-10-23 06:39:32 -0400

    Add docstrings to page functions to appease linter

--- app/app.py
+++ app/app.py
@@ -5,13 +5,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    """
+    Create the index for the website.
+    """
     return "<p>Index Page of the website</p>"
 
 @app.route("/home")
 def home_page():
+    """
+    Create the home page for the website.
+    """
     return "<p>This is the home page! Make sure the other routes are in different files!</p>"
 
 if __name__ == "__main__":
     db_helper = DatabaseManager()
     db_helper.get_all_data()
     db_helper.close_connection()
+    
