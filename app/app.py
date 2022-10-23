c80ffb66bd3a4533528a6fcdf6384c45b7454c60
Michael Kwan <michaelk.kwan@mail.utoronto.ca>
2022-10-23 06:19:40 -0400

    Setup basic flask app

--- app/app.py
+++ app/app.py
@@ -1,4 +1,15 @@
 from databasemanager import DatabaseManager
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "<p>Index Page of the website</p>"
+
+@app.route("/home")
+def home_page():
+    return "<p>This is the home page! Make sure the other routes are in different files!</p>"
 
 if __name__ == "__main__":
     db_helper = DatabaseManager()
