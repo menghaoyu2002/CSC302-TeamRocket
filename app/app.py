3bb37b669e36d7a7b5c0070891400db24390d035
Menghao Yu <68170909+menghaoyu2002@users.noreply.github.com>
2022-10-23 14:46:40 -0400

    Meeting4 notes (#31)
    * Setup basic flask app
    
    * Add docstrings to page functions to appease linter
    
    * Add tests to test the flask server
    
    * update docker to run flask server
    
    * remove running server in ci/cd
    
    * add meeting notes 4
    
    Co-authored-by: Michael Kwan <michaelk.kwan@mail.utoronto.ca>

--- app/app.py
+++ app/app.py
@@ -1,6 +1,19 @@
-from databasemanager import DatabaseManager
+from flask import Flask
 
-if __name__ == "__main__":
-    db_helper = DatabaseManager()
-    db_helper.get_all_data()
-    db_helper.close_connection()
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    """
+    Create the index for the website.
+    """
+    return "<p>Index Page of the website</p>"
+
+
+@app.route("/home")
+def home_page():
+    """
+    Create the home page for the website.
+    """
+    return "<p>This is the home page! Make sure the other routes are in different files!</p>"
