69f929eabf7c5731c9f2a9c226dafaf4bf16360a
Menghao Yu <68170909+menghaoyu2002@users.noreply.github.com>
2022-10-23 14:47:08 -0400

    Revert "Meeting4 notes (#31)" (#32)
    This reverts commit 3bb37b669e36d7a7b5c0070891400db24390d035.

--- app/app.py
+++ app/app.py
@@ -1,19 +1,6 @@
-from flask import Flask
+from databasemanager import DatabaseManager
 
-app = Flask(__name__)
-
-
-@app.route("/")
-def index():
-    """
-    Create the index for the website.
-    """
-    return "<p>Index Page of the website</p>"
-
-
-@app.route("/home")
-def home_page():
-    """
-    Create the home page for the website.
-    """
-    return "<p>This is the home page! Make sure the other routes are in different files!</p>"
+if __name__ == "__main__":
+    db_helper = DatabaseManager()
+    db_helper.get_all_data()
+    db_helper.close_connection()
--- scripts/run.sh
+++ scripts/run.sh
@@ -2,12 +2,10 @@
 
 echo 'starting the application...'
 {
-	docker run -it -p 5000:5000 --rm  --name team_rocket_app team_rocket_app
-	echo ""
+	docker run --rm --name team_rocket_app team_rocket_app
 } || {
 	echo 'Failed. There was an issue running the application with its docker image.'
 	echo 'Please report the issue to https://github.com/menghaoyu2002/CSC302-TeamRocket/issues with your terminal output'
 	exit 1
 }
-
 echo 'Application finished running.'
