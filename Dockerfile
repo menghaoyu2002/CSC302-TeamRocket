32002f1f3ef355b13887878d9b176a672742de4e
Michael Kwan <michaelk.kwan@mail.utoronto.ca>
2022-10-23 14:55:03 -0400

    Setup basic flask app (#30)
    * Setup basic flask app
    
    * Add docstrings to page functions to appease linter
    
    * Add tests to test the flask server
    
    * update docker to run flask server
    
    * remove running server in ci/cd
    
    * Remove testing and fix more linting issues
    
    * Remove requests from python requirements.txt
    
    Co-authored-by: Menghao Yu <menghaoyu2002@gmail.com>
    Co-authored-by: Menghao Yu <68170909+menghaoyu2002@users.noreply.github.com>

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ WORKDIR /usr/src/app
 COPY requirements.txt ./
 RUN pip install -r requirements.txt
 
+
 COPY . .
 
-CMD [ "python", "./app/app.py" ]
+CMD [ "python", "-m", "flask", "--app", "./app/app.py", "run", "--host","0.0.0.0"]
--- app/app.py
+++ app/app.py
@@ -2,14 +2,22 @@
 the main application
 """
 
-from pathlib import Path
-from pprint import pprint
-from databasemanager import DatabaseManager
-from constants import DATASET_PATH
+from flask import Flask
 
-if __name__ == "__main__":
-    db_helper = DatabaseManager()
-    db_helper.import_dataset(Path(__file__).parent / DATASET_PATH)
-    data = db_helper.get_all_data()
-    db_helper.close_connection()
-    pprint(data)
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    """
+    Create the index for the website.
+    """
+    return "<p>Index Page of the website</p>"
+
+
+@app.route("/home")
+def home_page():
+    """
+    Create the home page for the website.
+    """
+    return "<p>This is the home page! Make sure the other routes are in different files!</p>"
