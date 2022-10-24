1668f3ef0fbe5cf0d40554e5e6e2f3f4476a303b
Jeremiah Djianto <jeremiah.djianto@mail.utoronto.ca>
2022-10-23 23:48:18 -0400

    create app factory

--- app/app.py
+++ app/app.py
@@ -3,44 +3,68 @@ the main application
 """
 
 from pathlib import Path
-from flask import Flask, request
+from sqlite3 import Error
+from flask import Flask
 from constants import DATASET_PATH, DEFAULT_DATABASE
 from databasemanager import DatabaseManager
 
-app = Flask(__name__)
-app.config['DATABASE'] = DEFAULT_DATABASE
 
-db_manager = DatabaseManager(app.config['DATABASE'])
-path = Path(__file__).parent / DATASET_PATH
-db_manager.import_dataset(path)
+def create_app(test_config=None):
+    """App factory"""
+    app = Flask(__name__)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=DEFAULT_DATABASE,
+    )
 
+    if test_config is not None:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
 
-@app.route("/")
-def index():
-    """
-    Create the index for the website.
-    """
-    return "<p>Index Page of the website</p>"
-
-
-@app.route("/home")
-def home_page():
-    """
-    Create the home page for the website.
-    """
-    return "<p>This is the home page! Make sure the other routes are in different files!</p>"
+    db_manager = DatabaseManager(app.config['DATABASE'])
+    path = Path(__file__).parent / DATASET_PATH
+    db_manager.import_dataset(path)
 
 
-@app.route("/<string:name>/average", methods=["GET"])
-def get_average_undernourishment_by_name(name):
-    """Return the average undernourishment for the given country"""
-    data = db_manager.get_data_by_name(name)
+    @app.route("/")
+    def index():
+        """
+        Create the index for the website.
+        """
+        return "<p>Index Page of the website</p>"
 
-    undernourishments = [row_data.undernourishment for row_data in data]
 
-    return {
-        'data': {
-            'average': sum(undernourishments) / len(undernourishments) \
-                if undernourishments != [] else 0
-        }
-    }, 200
+    @app.route("/home")
+    def home_page():
+        """
+        Create the home page for the website.
+        """
+        return "<p>This is the home page! Make sure the other routes are in different files!</p>"
+
+
+    @app.route("/<string:name>/average", methods=["GET"])
+    def get_average_undernourishment_by_name(name):
+        """Return the average undernourishment for the given country"""
+        try:
+            data = db_manager.get_data_by_name(name)
+        except Error as error:
+            return {
+                'error': {
+                    f'Error fetching data: {error}'
+                }
+            }, 500
+
+        undernourishments = [row_data.undernourishment for row_data in data]
+
+        if undernourishments != []:
+            return {
+                'data': {
+                    'average': sum(undernourishments) / len(undernourishments)
+                }
+            }, 200
+
+        return {
+            'error': 'No data found for name: ' + name
+        }, 404
+
+    return app
--- tests/test_average_undernourishment.py
+++ tests/test_average_undernourishment.py
@@ -3,31 +3,36 @@
 import os
 import tempfile
 import pytest
-from app import app
+from app import create_app
 
 
 class TestAverageUndernourishment:
     """Class for testing the get_average_undernourishment_by_name endpoint"""
+
     @pytest.fixture
     def client(self):
         """Fixture to be called before each test to set up app interface"""
-        db_fd, app.config['DATABASE'] = tempfile.mkstemp()
-        app.config['TESTING'] = True
+        db_fd, db_path = tempfile.mkstemp()
+
+        app = create_app({
+            'TESTING': True,
+            'DATABASE': db_path,
+        })
 
         with app.test_client() as client:
             yield client
 
         os.close(db_fd)
-        os.unlink(app.config['DATABASE'])
+        os.unlink(db_path)
 
     def test_get_average_undernourishment_by_name(self, client):
         """Tests get_average_undernourishment_by_name() to return correct average."""
-        res = client.get('/World/average').get_json()
-        print(res)
-        assert 10.168420992399517 == res['data']['average']
+        res = client.get('/World/average')
+        assert 10.168420992399517 == res.get_json()['data']['average']
+        assert res.status_code == 201
 
     def test_get_average_undernourishment_by_name_none(self, client):
-        """Tests get_average_undernourishment_by_name() to return correct average when name
+        """Tests get_average_undernourishment_by_name() to return error when name
         doesn't exist."""
-        res = client.get('/Lalaland/average').get_json()
-        assert 0 == res['data']['average']
+        res = client.get('/Lalaland/average')
+        assert res.status_code == 404
