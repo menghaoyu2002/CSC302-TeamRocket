8c93e3c44a2e1b9c9d025ad575f5d9e37fc13ee0
Menghao Yu <menghaoyu2002@gmail.com>
2022-10-25 16:28:48 -0400

    add get undernourishment by name endpoint

--- app/routes/data_route.py
+++ app/routes/data_route.py
@@ -9,16 +9,36 @@ from databasemanager import DatabaseManager
 data_blueprint = Blueprint("data", __name__)
 
 
+@data_blueprint.route('/<string:name>', methods=["GET"])
+def get_undernourishment_by_name(name):
+    try:
+        db_manager = DatabaseManager(current_app.config['DATABASE'])
+        data = db_manager.get_data_by_name(name.lower())
+        if len(data) == 0:
+            return {
+                'error': {
+                    'msg': 'No entry with the name ' + name
+                }}, 404
+        return {'data': data}, 200
+    except Error as error:
+        return {
+            'error': {
+                'msg': f'Error fetching data: {error}'
+            }
+        }, 500
+
+
 @data_blueprint.route("/<string:name>/average", methods=["GET"])
 def get_average_undernourishment_by_name(name):
     """Return the average undernourishment for the given country"""
     db_manager = DatabaseManager(current_app.config['DATABASE'])
     try:
         data = db_manager.get_data_by_name(name.lower())
+        db_manager.close_connection()
     except Error as error:
         return {
             'error': {
-                f'Error fetching data: {error}'
+                'msg': f'Error fetching data: {error}'
             }
         }, 500
 
