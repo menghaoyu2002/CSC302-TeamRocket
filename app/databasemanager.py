07fd4a227b36520372a9fc2be785f416a0bd2861
Menghao Yu <menghaoyu2002@gmail.com>
2022-10-25 17:14:58 -0400

    update docstring

--- app/databasemanager.py
+++ app/databasemanager.py
@@ -106,7 +106,7 @@ class DatabaseManager:
         return result if result is None else result[0]
 
     def get_data_by_name(self, name: str) -> List[RowData]:
-        """Returns a list of RowData object that have name as their RowData.entity."""
+        """Returns a list of RowData object that have name as their RowData.name."""
         params = (name,)
         cur = self.connection.cursor()
         query = f'SELECT * FROM {Table.NAME} WHERE {Table.COLUMN_ENTITY}=? ORDER BY year'
