212567c1196f558a6ac7254e114df1a738debc31
Menghao Yu <menghaoyu2002@gmail.com>
2022-10-31 02:06:30 -0400

    Revert "refactor all python files into server folder"
    This reverts commit 804e392ec4bda3548df73d3ddf0a4d0e6868c9ef.

--- app/constants.py
+++ app/constants.py
@@ -1,4 +1,4 @@
 """Constant values for the application"""
 
 DEFAULT_DATABASE = 'sqlitedatabase.db'
-DATASET_PATH = './datasets/prevalence-of-undernourishment.csv'
+DATASET_PATH = 'datasets/prevalence-of-undernourishment.csv'
--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 
 echo 'building docker image...'
 { 
-	docker build -t team_rocket_app ./server
+	docker build -t team_rocket_app .
 } || {
 	echo 'docker build failed.'
 	echo 'Please report the issue to https://github.com/menghaoyu2002/CSC302-TeamRocket/issues with your terminal output'
