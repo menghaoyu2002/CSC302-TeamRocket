bdec327a36c85f71b7015ec90453673fd6371123
Menghao Yu <menghaoyu2002@gmail.com>
2022-11-01 17:20:26 -0400

    use docker compose and update github ci/cd to use docker compose

---
+++ scripts/test.sh
@@ -0,0 +1,4 @@
+echo "Running all tests"
+
+./server/scripts/test.sh
+./client/scripts/test.sh
--- server/scripts/test.sh
+++ server/scripts/test.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-echo 'Preparing to run tests'
+echo 'Preparing to run server tests'
 {
-	docker run --rm --name team_rocket_server team_rocket_server pytest
+	docker run --rm --name csc302-teamrocket-server csc302-teamrocket-server pytest -v 
 } || {
-	echo 'Tests could not be run.'
+	echo 'Server tests could not be run.'
 	echo 'Report this issue to https://github.com/menghaoyu2002/CSC302-TeamRocket/issues with your terminal output'
 	exit 1
 }
 
-echo 'Tests have been successfully run'
+echo 'Server tests have been successfully run'
 
