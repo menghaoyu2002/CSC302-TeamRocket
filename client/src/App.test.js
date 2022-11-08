113699e60f0e960339904db4ca562b8de4ca1545
Jeremiah Djianto <jeremiah.djianto@mail.utoronto.ca>
2022-11-08 01:20:14 -0500

    fix App.test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,6 +1,22 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
+const { ResizeObserver } = window;
+
+beforeEach(() => {
+  delete window.ResizeObserver;
+  window.ResizeObserver = jest.fn().mockImplementation(() => ({
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+    disconnect: jest.fn(),
+  }));
+});
+
+afterEach(() => {
+  window.ResizeObserver = ResizeObserver;
+  jest.restoreAllMocks();
+});
+
 test("renders input labels", () => {
   render(<App />);
   const linkElement = screen.getByText("Name:");
