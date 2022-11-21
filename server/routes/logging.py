033525db13b406915a4a669f2e0b776655ea146d
Menghao Yu <menghaoyu2002@gmail.com>
2022-11-21 15:56:50 -0500

    Log start and end of request

--- server/routes/logging.py
+++ server/routes/logging.py
@@ -1,6 +1,7 @@
 """The module for logging requests"""
 import datetime
 import time
+from typing import Tuple, List
 from flask import Blueprint, current_app, g, request
 
 
@@ -8,13 +9,31 @@ logging_blueprint = Blueprint("logger", __name__)
 
 
 @logging_blueprint.before_app_request
-def start_timer():
+def log_request_start():
     """Start a timer for how long the request takes"""
     g.start = time.time()
+    now = time.time()
+    timestamp = datetime.datetime.fromtimestamp(now).isoformat()
+
+    ip_address = request.headers.get('X-Forwarded-For', request.remote_addr)
+    host = request.host.split(':', 1)[0]
+    args = dict(request.args)
+
+    log_params = [
+        ('method', request.method),
+        ('path', request.path),
+        ('time', timestamp),
+        ('ip', ip_address),
+        ('host', host),
+        ('params', args),
+        ('body', request.get_json(silent=True)),
+    ]
+
+    current_app.logger.info('received request: ' + parse_tuples_to_line(log_params))
 
 
 @logging_blueprint.after_app_request
-def log_request(response):
+def log_request_end(response):
     """Log every request and the data passed in each request"""
     now = time.time()
     duration = round(now - g.start, 2)
@@ -41,11 +60,14 @@ def log_request(response):
     if request_id:
         log_params.append(('request_id', request_id))
 
+    current_app.logger.info('completed request: ' +
+                            parse_tuples_to_line(log_params))
+
+    return response
+
+
+def parse_tuples_to_line(log_params: List[Tuple[str, any]]):
     parts = []
     for name, value in log_params:
         parts.append(f"{name}={value}")
-    line = " ".join(parts)
-
-    current_app.logger.info(line)
-
-    return response
+    return " ".join(parts)
