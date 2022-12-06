981111209afb80d756071d67b0224a7508b75811
Menghao Yu <menghaoyu2002@gmail.com>
2022-12-06 15:03:26 -0500

    add base url

--- client/cypress.config.js
+++ client/cypress.config.js
@@ -1,7 +1,8 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:5000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
