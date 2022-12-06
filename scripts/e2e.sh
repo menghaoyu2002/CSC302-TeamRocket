c06f1f47b16ae477224f358ef07308620eac30a5
Menghao Yu <menghaoyu2002@gmail.com>
2022-12-06 14:07:16 -0500

    extract e2e tests to different file

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -2,8 +2,7 @@
 
 echo 'Preparing to run end to end tests'
 {
-	cd client
-	npm run e2e
+	docker compose -f docker-compose.cypress.yml up --build --abort-on-container-exit
 } || {
 	echo 'end to end tests could not be run.'
 	echo 'Report this issue to https://github.com/menghaoyu2002/CSC302-TeamRocket/issues with your terminal output'
