854de7fec8ab386ccc11b401901060adb5e68b71
Menghao Yu <menghaoyu2002@gmail.com>
2022-12-06 14:32:06 -0500

    allow container to be stopped

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,7 +2,7 @@
 timestamp=$(date +%Y-%m-%dT%H:%M:%S%:z)
 {
 	mkdir -p ./logs
-	docker compose up --build --abort-on-container-exit | tee ./logs/$timestamp.log
+	docker compose up --build | tee ./logs/$timestamp.log
 } || {
 	echo 'Unable to run program.'
 	echo 'Please report this issue to https://github.com/menghaoyu2002/CSC302-TeamRocket/issues with the log file:'
